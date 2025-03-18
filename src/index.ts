b97f69fe6afd76984933f19f3e1aaf6ff5ced784
Nishant <69218488+NishantCoder108@users.noreply.github.com>
2025-03-18 22:00:27 +0530

    Bidirectional websocket implement by room id from different user

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,80 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const subscriptions: {
+  [key: string]: {
+    ws: WebSocket;
+    rooms: string[];
+  };
+} = {};
+
 wss.on("connection", function connection(userSocket) {
-  userSocket.on("error", console.error);
+  //   userSocket.on("error", console.error);
 
+  const id = randomId();
+
+  subscriptions[id] = {
+    ws: userSocket,
+    rooms: [],
+  };
+
+  /*
+
+  -> For subscribe:
+
+  {
+    "type":"SUBSCRIBE",
+    "room": "room1"
+}
+
+-> For sendMessage:
+
+{
+  "type":"sendMessage",
+  "message": "Hello",
+  "roomId": "room1"
+}
+
+
+So, for every message, we need to send it to all the users who are subscribed to that room.
+- i will take roomId from userSocket and send it to all the users who are subscribed to that room.
+
+*/
+
+  setInterval(() => {
+    console.log("subscriptions", subscriptions);
+  }, 5000);
   userSocket.on("message", function message(data) {
-    console.log("received: %s", data);
+    console.log("data", data);
+    const parsedMessage = JSON.parse(data as unknown as string);
 
-    userSocket.send("You sent me :" + data);
+    if (parsedMessage.type === "SUBSCRIBE") {
+      if (subscriptions[id].rooms.includes(parsedMessage.room)) {
+        return;
+      }
+      subscriptions[id].rooms.push(parsedMessage.room);
+    }
+
+    if (parsedMessage.type === "sendMessage") {
+      const message = parsedMessage.message;
+      const roomId = parsedMessage.roomId;
+
+      Object.keys(subscriptions).forEach((userId) => {
+        if (subscriptions[userId].rooms.includes(roomId)) {
+          subscriptions[userId].ws.send(
+            JSON.stringify({
+              type: "message",
+              message,
+              roomId,
+            })
+          );
+        }
+      });
+    }
   });
-
-  userSocket.send("something");
 });
+
+const randomId = () => {
+  return Math.random();
+};
